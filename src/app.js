68befcb847fda120c5a05c695b309e124cabc860
itsamanverma <amanvermame786@gmail.com>
2020-11-05 22:36:49 +0530

    landing page design

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import Jumbotron from './components/jumbotron';
-function App() {
+import data from './Json/jumbo.json';
+
+
+const App = () => {
   return (
     <div className="App">
-      <Jumbotron />
+      <Jumbotron.Container>
+        {data.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
     </div>
   );
 }
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Inner } from './style/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image } from './style/jumbotron';
 
-export default function Jumbotron({ }) {
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
-        <Inner>
-            <p>I am the Jumbo, Hello Again!!</p>
+        <Inner direction={direction}>
+            <p>{children}</p>
         </Inner>
     )
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps} />;
 }
